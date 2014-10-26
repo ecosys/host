e6ce25b209b3f7d9eafe05be56ec200982f062f2
ecosys <ecosys13@gmail.com>
2014-10-26 10:59:43 -0500

    - restart

--- app.go
+++ app.go
@@ -10,6 +10,7 @@ func main() {
 	cfg := conf.GetConfig()
 	log.Printf("cfg: %v\n", cfg)
 	count := len(cfg.Modules)
+	log.Printf("count: %v - %v\n", count, cfg.Modules)
 	for {
 		if len(cfg.Modules) != count {
 			log.Printf("new count: %v\n", count)
